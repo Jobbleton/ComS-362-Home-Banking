f10b3f48264353dbcb48d19ad37b6626c10af518
Joe Randazzo <joey@iastate.edu>
2016-02-08 18:34:25 -0600

    Further Initialization of Home Banking Interface
    + added window
    + added main pane
    + added subpanel

--- src/HomeBankingView.java
+++ src/HomeBankingView.java
@@ -1,4 +1,69 @@
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.util.ArrayList;
 
-public class HomeBankingView {
+import javax.imageio.ImageIO;
+import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
+public class HomeBankingView extends JFrame
+{
+	private static final long serialVersionUID = 1L;
+	Image storedImage;
+	private JPanel mainContentPane;
+
+	//Launch the application.
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+					HomeBankingView frame = new HomeBankingView();
+					frame.setVisible(true);
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	//Create the frame.
+	public HomeBankingView() {
+		//block for main window
+		setTitle("ComS 362 Home Banking");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, 1280, 720);
+		mainContentPane = new JPanel();
+		mainContentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(mainContentPane);
+		mainContentPane.setLayout(null);
+		
+		//image panel for facemapped image
+		JPanel largeInternalPanel = new JPanel();
+		largeInternalPanel.setBackground(SystemColor.controlHighlight);
+		largeInternalPanel.setBounds(10, 11, 1244, 659);
+		mainContentPane.add(largeInternalPanel);
+		largeInternalPanel.setLayout(null);
+	}
 }
+
